4f3d1ac99fd848afff92e0591130dc62dba9233e
João Victor <jotav.lim4@gmail.com>
2025-02-21 14:53:03 -0300

    Correção no código do exercício 3

--- Ch1/ex03.py
+++ Ch1/ex03.py
@@ -1,3 +1,3 @@
-print("pace médio em minutos/milha:", (42 + 42/60)/(10*0.621371))
-print("pace médio em minutos/segundo:", (42*60 + 42)/(10*0.621371))
-print("velocidade média em milhas por hora:", (10*0.621371)/((42 + 42/60)/60))
+print("pace médio em minutos/milhas:", (42 + 42/60)/(10*0.621371))
+print("pace médio em segundos/milhas:", (42*60 + 42)/(10*0.621371))
+print("velocidade média em milhas/hora:", (10*0.621371)/((42 + 42/60)/60))
