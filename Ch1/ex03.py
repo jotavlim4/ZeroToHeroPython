f4b5f8a06f93b3a18432a123d4c3517eae1ffec7
João Victor <jotav.lim4@gmail.com>
2025-02-21 14:51:36 -0300

    Soluções dos exercícios do capítulo 1

---
+++ Ch1/ex03.py
@@ -0,0 +1,3 @@
+print("pace médio em minutos/milha:", (42 + 42/60)/(10*0.621371))
+print("pace médio em minutos/segundo:", (42*60 + 42)/(10*0.621371))
+print("velocidade média em milhas por hora:", (10*0.621371)/((42 + 42/60)/60))
