d66af712a66097e620a9763adde0c1c547a75f13
João Victor <jotav.lim4@gmail.com>
2025-03-19 22:31:48 -0300

    Atulização de exercício

--- Code/square_functions_and_docstring.py
+++ Code/square_functions_and_docstring.py
@@ -4,9 +4,9 @@ import turtle
 bob = turtle.Turtle()
 
 def square(t, lenght):
-	''' 
+	'''
 	parametros: t, um objeto tipo Turtle e lenght, um valor positivo que representa o tamanho do quadrado.
-	retorno: um quadrado de lado lenght. 
+	retorno: um quadrado de lado lenght.
 	'''
 	for i in range(4):
 		t.fd(lenght)
@@ -35,7 +35,7 @@ def arc(turtle, radius, angle):
 		recebe um objeto turtle, um valor positivo para o raio do circulo e um valor positivo para o angulo.
 		retorna um arco de uma circufenrecia de raio dado e angulo angle.
 	"""
-	arc_lenght =  2 * math.pi * radius * (angle / 360) 
+	arc_lenght =  2 * math.pi * radius * (angle / 360)
 	n = int(arc_lenght / 3) + 1
 	step_lenght = arc_lenght / n
 	step_angle = float(angle) / n
@@ -49,6 +49,6 @@ def circle(turtle, radius):
 	"""
 	arc(turtle, radius, angle = 360)
 
-
+circle(bob, 100)
 
 turtle.mainloop()
