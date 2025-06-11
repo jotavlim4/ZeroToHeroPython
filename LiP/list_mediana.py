217396335bb2d69f62ba88dfe61b5ddf79d73a45
João Victor <jotav.lim4@gmail.com>
2025-06-11 14:54:27 -0300

    Ajuste na solução

--- LiP/list_mediana.py
+++ LiP/list_mediana.py
@@ -1,21 +1,24 @@
 def bs(l):
 	for i in range(len(l)):
 		for j in range(i + 1):
-			if l[i] > l[j]:
+			if l[i] < l[j]:
 				l[i], l[j] = l[j], l[i]
-
 
 n = int(input())
 l = list()
 
-for i in range(len(l)):
+for i in range(n):
 	number = int(input())
 	l.append(number)
 
 bs(l)
 
+even_len_1 = (len(l))//2
+even_len_2 = (len(l))//2 - 1
+odd_len = len(l)//2
+
 if len(l) % 2 == 0:
-	print(f"Mediana: {l[len/2] + l[len/2 -1]}")
+	print(f"Mediana: {(l[even_len_1] + l[even_len_2])/2:.1f}")
 else:
-	print(f"Mediana: {l[len/2]:.1f}")
+	print(f"Mediana: {l[odd_len]:.1f}")
 
